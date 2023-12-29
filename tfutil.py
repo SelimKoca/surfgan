c84f64a4eb51e5e95c37074f947ce4df56a4d245
SelimKoca <94684570+SelimKoca@users.noreply.github.com>
2023-12-29 23:11:51 +0000

    Update tfutil.py

--- tfutil.py
+++ tfutil.py
@@ -12,7 +12,7 @@ import importlib
 import imp
 import numpy as np
 from collections import OrderedDict
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 #----------------------------------------------------------------------------
 # Convenience.
