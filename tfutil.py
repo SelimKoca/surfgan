e21c7e9f1f9abf144b9cae072563a36939bd51e1
SelimKoca <94684570+SelimKoca@users.noreply.github.com>
2024-01-04 14:14:25 +0000

    Update tfutil.py

--- tfutil.py
+++ tfutil.py
@@ -247,7 +247,7 @@ class Optimizer:
     def __init__(
         self,
         name                = 'Train',
-        tf_optimizer        = 'tf.train.Adam',
+        tf_optimizer        = 'tf.optimizers.Adam',
         learning_rate       = 0.001,
         use_loss_scaling    = False,
         loss_scaling_init   = 64.0,
