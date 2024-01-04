dcead80e321b13798d24740abf503b41d4440c5e
SelimKoca <94684570+SelimKoca@users.noreply.github.com>
2024-01-04 20:21:36 +0000

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@
 import os
 import glob
 import numpy as np
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import tfutil
 
 #----------------------------------------------------------------------------
